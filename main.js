b2f748a50dd2bfaee18424fc907e0566c0e5d21d
Valentyn Kliuchev <valentynkliuchev@gmail.com>
2025-02-12 03:52:55 +0200

    Added sounds, win condition

--- main.js
+++ main.js
@@ -1,42 +1,84 @@
 "use strict";
+
+const pickUpSound = new Audio('/audio/PICKUP.wav');
+const putDownSound = new Audio('/audio/PUTDOWN.wav');
+const winSound = new Audio('/audio/BOXBELL.wav');
+pickUpSound.volume = 0.1;
+putDownSound.volume = 0.1;
+winSound.volume = 0.1;
+
+
+const playSound = (sound) => {
+  sound.currentTime = 0; // Reset sound to start
+  sound.play();
+};
+
+
+const score = document.getElementById('score');
+let pieces; 
 let activePiece;
-document.addEventListener('mousedown', (e) => {
+
+
+document.addEventListener('mousedown', handleMouseDown);
+document.addEventListener('mouseup', handleMouseUp);
+document.addEventListener('mousemove', handleMouseMove);
+
+function handleMouseDown(e) {
     e.preventDefault(); // Drag bugfix    
     if(e.target.classList.contains('puzzle-piece')) {
+        playSound(pickUpSound);
         activePiece = e.target;
         activePiece.style.pointerEvents = 'none';
     } else {
         return
     }
-})
-document.addEventListener('mouseup', (e) => {
-        if (!activePiece) return
-        if(e.target.classList.contains('puzzle-piece-slot')) {
-            const slotPosition = e.target.getBoundingClientRect();
-            const prevPositionY = activePiece.style.top;
-            const prevPositionX = activePiece.style.left;
-            activePiece.style.position = 'absolute';
-            activePiece.style.top = `${slotPosition.top}px`
-            activePiece.style.left = `${slotPosition.left}px`           
-            activePiece.style.pointerEvents = 'auto';  
-             
-            if(parseInt(prevPositionY) + parseInt(prevPositionX) != (slotPosition.top + slotPosition.left).toFixed(0)) {
-                updateScore()
-            }
-        } else  {
-            activePiece.style.pointerEvents = 'auto';
-            activePiece.style.position = 'relative';
-            activePiece.style.top = `0px`
-            activePiece.style.left = `0px`
+}
+
+function handleMouseUp(e) {
+    if (!activePiece) return
+    if(e.target.classList.contains('puzzle-piece-slot')) {
+        const slotPosition = e.target.getBoundingClientRect();
+        const prevPositionY = parseInt(activePiece.style.top);
+        const prevPositionX = parseInt(activePiece.style.left);
+        activePiece.style.position = 'absolute';
+        activePiece.style.top = `${slotPosition.top}px`
+        activePiece.style.left = `${slotPosition.left}px`           
+        activePiece.style.pointerEvents = 'auto';
+
+        if (activePiece.dataset.order === e.target.dataset.order) {
+            activePiece.dataset.placed = "true"; 
+        } else {
+            activePiece.dataset.placed = "false";
         }
-        activePiece = null;
-})
-document.addEventListener('mousemove', (e) => {
+         
+        if (prevPositionY !== Math.round(slotPosition.top) || prevPositionX !== Math.round(slotPosition.left)) {
+            updateScore();
+        }
+    } else  {
+        activePiece.dataset.placed = "false";
+        activePiece.style.pointerEvents = 'auto';
+        activePiece.style.position = 'relative';
+        activePiece.style.top = `0px`
+        activePiece.style.left = `0px`
+    }
+    
+    playSound(putDownSound);
+    activePiece = null;
+    if(checkWin()) {
+        playSound(winSound);
+        score.textContent += ` Congratulations!`
+        document.removeEventListener('mousedown', handleMouseDown);
+        document.removeEventListener('mouseup', handleMouseUp);
+        document.removeEventListener('mousemove', handleMouseMove);
+    }
+}
+
+function handleMouseMove(e) {
     if (!activePiece) return
     activePiece.style.position = 'absolute';
     activePiece.style.top = `${e.pageY - 42}px`;
     activePiece.style.left = `${e.pageX - 42}px`;
-})
+}
 
 function generatePieces() {
     const puzzlePieceBin = document.querySelector('.puzzle-piece-bin');
@@ -47,6 +89,7 @@ function generatePieces() {
     for(let i = 0; i < gridSize; i++){
         const piece = document.createElement('div');
         piece.classList.add('puzzle-piece');
+        piece.dataset.order = i+1;
         piece.style.backgroundImage = 'url(images/monroe.jpg)';
         piece.style.backgroundSize = '425px';
         const row = Math.floor(i / cols);
@@ -55,6 +98,8 @@ function generatePieces() {
         puzzlePieces.push(piece);
     }
     puzzlePieceBin.append(...shuffleArray(puzzlePieces));
+
+    pieces = document.querySelectorAll('.puzzle-piece');
 }
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -74,77 +119,10 @@ window.addEventListener('resize', () => {
 })
 
 function updateScore() {
-    const score = document.getElementById('score');
     score.textContent++ 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const container = document.querySelector(".container");
-// const puzzleImage = document.getElementById("puzzle-image");
-// const puzzleInput = document.getElementById("puzzle-file-input");
-// const puzzlePatternButton = document.getElementById("puzzle-pattern-button");
-
-// puzzleInput.addEventListener("change", (e) => updatePuzzleImage(e));
-// puzzlePatternButton.addEventListener("click", createPuzzlePattern);
-
-// function updatePuzzleImage(event) {
-//   const image = URL.createObjectURL(event.target.files[0]);
-//   puzzleImage.src = image;
-// }
-// function createPuzzlePattern() {
-//   const gridSize = 5;
-//   const pieceWidth = puzzleImage.width / gridSize;
-//   const pieceHeight = puzzleImage.height / gridSize;
-//   const grid = document.createElement("div");
-//   grid.classList.add("puzzle-grid");
-//   grid.style.display = `grid`;
-
-//   grid.style.height = `${puzzleImage.height}px`;
-//   grid.style.width = `${puzzleImage.width}px`;
-//   grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-//   grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;  
-//   const puzzlePieces = [];
-//   for (let i = 0; i < gridSize * gridSize; i++) {
-//     const piece = document.createElement('div');
-//     piece.classList.add('puzzle-piece');
-//     piece.style.width = `${pieceWidth}px`;
-//     piece.style.height = `${pieceHeight}px`;
-//     piece.style.backgroundImage = `url(${puzzleImage.src})`
-//     const row = Math.floor(i / gridSize);
-//     const col = i % gridSize;
-//     piece.style.backgroundPosition = `-${col * pieceWidth}px -${row * pieceHeight}px`;
-
-//     piece.style.outline = '2px solid black'
-//     puzzlePieces.push(piece)
-//   }
-//   grid.append(...puzzlePieces)
-//   console.log(puzzlePieces);
-  
-//   container.insertAdjacentElement("afterbegin", grid);
-// }
+function checkWin() {
+    return Array.from(pieces).every(piece => piece.dataset.placed === "true");
+}
